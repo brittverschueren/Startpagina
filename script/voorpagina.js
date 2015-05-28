08d1b57376c4b49a04230148f310e6f22da6e6e5
brittverschueren <brittverschueren1996@hotmail.com>
2015-05-28 15:52:34 +0200

    transformation toegevoegd

--- script/voorpagina.js
+++ script/voorpagina.js
@@ -4,10 +4,14 @@ var main = function () {
 }
 	$("img").on("mouseover", function(){
 		$(this).closest(".logo").css("opacity","0.8");
+		$(this).closest(".logo").css("transform", "rotateY(180deg)");
+		$(this).closest(".logo").css("transition-duration", "2s");
+});
 		
-	});
+
 	$("img").on("mouseout", function(){
 		$(this).closest(".logo").css("opacity","1");
-		
+		$(this).closest(".logo").css("transform", "rotateY(360deg)");
+		$(this).closest(".logo").css("transition-duration", "2s");
 	});
 $("document").ready(main);
